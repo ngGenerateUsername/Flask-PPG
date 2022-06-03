30e0641d110ed24053b2d8a153f0e484aff3d30a
sispoof <mbenhamouda099@gmail.com>
2022-06-03 14:37:36 +0100

    fixing bug

--- App/__init__.py
+++ App/__init__.py
@@ -20,7 +20,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = "login_admin"
+login_manager.login_view = "login_direc"
 login_manager.login_message_category = "info"
 
 from App import routes
